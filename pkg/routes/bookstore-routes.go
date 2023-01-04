3a53c4f564f051d217e3af203d6e8ccccc33620a
kaushikyelne <kaushik.s.yelne@gmail.com>
2023-01-04 16:55:09 +0530

    CRUD functions added

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -8,8 +8,8 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.GetByBookId).Methods("GET")
-	router.HandleFunc("/book/{bookId}", controllers.UpdateDelete).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 
 }
